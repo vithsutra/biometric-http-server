0b48e3363ce409405f39798ca8588ad8110624ce
Magowtham <magowtham7@gmail.com>
2025-03-04 00:21:07 +0530

    time converted updated

--- pkg/utils/time_converter.go
+++ pkg/utils/time_converter.go
@@ -3,6 +3,10 @@ package utils
 import "time"
 
 func ConvertTo12HourFormat(timeStr string) (string, error) {
+
+	if timeStr == "25:00" {
+		return timeStr, nil
+	}
 	// Parse the input time string in 24-hour format
 	t, err := time.Parse("15:04", timeStr)
 	if err != nil {
