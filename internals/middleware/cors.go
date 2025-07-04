786e83797565ffbe4acda132bdb2e61e48e15ab2
Vithsutra Tech <vithsutra.tech@gmail.com>
2025-07-04 19:58:22 +0530

    Update cors.go

--- internals/middleware/cors.go
+++ internals/middleware/cors.go
@@ -22,7 +22,7 @@ func CorsMiddleware(ah http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000", "https://biometric.admin.vithsutra.com")                          // Allow all origins
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")                          // Allow all origins
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")               // Allow specific methods
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With") // Allow headers
 		w.Header().Set("Access-Control-Allow-Credentials", "true")                                      // Allow cookies (if needed)
